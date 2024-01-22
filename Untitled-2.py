7e98cd31c4859be23fbbe21c242b8b1329c5afd7
phuchung12tn4 <phuchung12tn4@gmail.com>
2024-01-22 20:09:23 +0700

    4

---
+++ Untitled-2.py
@@ -0,0 +1,8 @@
+from array import *
+array1 = array('i',[1,2,3,4,5,6])
+def accessElement(array, index):
+    if index > len(array):
+        print("There is not any element in this index")
+    else:
+        print(array[index])
+accessElement(array1, 8)
